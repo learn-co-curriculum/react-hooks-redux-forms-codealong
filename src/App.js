5ff51eb5ba985ba46c79e77ae05a1313ab1f9fb3
maxwell <github email address>
2018-07-25 07:20:35 -0400

    updated readme for react-redux

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CreateTodo store={this.props.store} />
+        <CreateTodo />
       </div>
     );
   }
--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,14 +1,8 @@
 export default function manageTodo(state = {
   todos: [],
 }, action) {
-  switch (action.type) {
-    case 'ADD_TODO':
 
-      console.log({ todos: state.todos.concat(action.payload.text) });
+  console.log("reducer received this action:", action);
 
-      return { todos: state.todos.concat(action.payload.text) };
-
-    default:
-      return state;
-  }
+  return state;
 }
